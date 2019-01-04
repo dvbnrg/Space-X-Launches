928b9d185c89f4b9b2a7e5604adcabf55fafb855
Dave Banerjee <justdave@Daves-MacBook-Pro.local>
2019-01-04 11:31:04 -0600

    updated graphql proxy

---
+++ public/precache-manifest.fd31507efdadefccce57d87255de0e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a2bd1df5b110cc16ee",
+    "url": "/static/css/main.9438f3a3.chunk.css"
+  },
+  {
+    "revision": "c7a2bd1df5b110cc16ee",
+    "url": "/static/js/main.c7a2bd1d.chunk.js"
+  },
+  {
+    "revision": "5be207921493f3117b0d",
+    "url": "/static/js/1.5be20792.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05288073e80123ddfab10bc48dbab0b2",
+    "url": "/static/media/wolf.05288073.svg"
+  },
+  {
+    "revision": "c09f6ab988c857c9f9022d554e8a50e6",
+    "url": "/index.html"
+  }
+];
